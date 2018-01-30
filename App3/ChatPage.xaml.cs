a855cbfc1ae6f2d7d8e2e3686a3fa28ae2343ecb
Firefly74940 <swat74@hotmail.fr>
2018-01-31 00:00:51 +0100

    Added Emojies support Added refresh after post + clear field before to avoid double send Restructured message parsing for futur support if images ( thumbs up too )

--- App3/ChatPage.xaml.cs
+++ App3/ChatPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -22,13 +23,24 @@ using HtmlAgilityPack;
 
 namespace App3
 {
+    public enum MessageTypes
+    {
+        Self,
+        Other,
+        None,
+        Img,
+        Link,
+    }
     public class ChatMessage
     {
+        public MessageTypes MessageType { get; set; }
         public string DisplayName { get; set; }
         public string UserID { get; set; }
         public string Message { get; set; }
         public override string ToString()
         {
+            if (MessageType == MessageTypes.None)
+                return Message;
             return DisplayName + ": " + Message;
         }
     }
@@ -75,17 +87,19 @@ namespace App3
                 }
             }
         }
-        private async void RefreshConversation(ChatHeader header)
+        private async void RefreshConversation(ChatHeader header, HtmlDocument htmlDoc = null)
         {
-            var htmlDoc = await MainPage.GetHtmlDoc(header.Href);
+            if (htmlDoc == null)
+                htmlDoc = await MainPage.GetHtmlDoc(header.Href);
 
             GetSubmitForm(htmlDoc);
-            var nodes = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"messageGroup\"]/div[2]/div");
-            foreach (var node in nodes)
+            var messagePackNodes = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"messageGroup\"]/div[2]/div");
+            foreach (var messagePackNode in messagePackNodes)
             {
+
                 var MessageUsername = "";
                 var MessageDisplayUsername = "";
-                var NameNode = node.SelectSingleNode("div[1]/a");
+                var NameNode = messagePackNode.SelectSingleNode("div[1]/a");
                 if (NameNode != null)
                 {
                     var namelink = NameNode.GetAttributeValue("href", "");
@@ -93,17 +107,117 @@ namespace App3
                     MessageUsername = namelink.Substring(1, end - 1);
                     MessageDisplayUsername = HtmlEntity.DeEntitize(NameNode.InnerText);
                 }
-                var TextNodes = node.SelectNodes("div[1]/div");
-                if (TextNodes != null)
+
+
+                var messageNodes = messagePackNode.SelectNodes("div[1]/div");
+                if (messageNodes != null)
                 {
-                    foreach (var textNode in TextNodes)
+                    foreach (var messageNode in messageNodes)
                     {
-                        listView.Items.Add(new ChatMessage() { Message = HtmlEntity.DeEntitize(textNode.InnerText), UserID = MessageUsername, DisplayName = MessageDisplayUsername });
+                        #region MessageText
+                        if (messageNode.FirstChild.Name == "span")
+                        {
+                            string buildedMessage = "";
+
+                            foreach (var textNode in messageNode.FirstChild.ChildNodes)
+                            {
+                                if (textNode.Name == "#text")
+                                {
+                                    buildedMessage += textNode.InnerText;
+                                }
+                                else if (textNode.Name == "i")
+                                {
+                                    var isrc = textNode.GetAttributeValue("style", "");
+                                    if (!string.IsNullOrEmpty(isrc))
+                                    {
+                                        var nameStart = isrc.LastIndexOf('/');
+                                        var nameEnd = isrc.LastIndexOf('.');
+
+                                        if (nameStart != -1 && nameEnd != -1)
+                                        {
+                                            var name = isrc.Substring(nameStart + 1, nameEnd - nameStart - 1);
+                                            var x = char.ConvertFromUtf32(int.Parse(name, NumberStyles.HexNumber));
+                                            buildedMessage += x;
+                                        }
+
+                                    }
+                                }
+                                else if (textNode.Name == "img")
+                                {
+                                    var isrc = textNode.GetAttributeValue("src", "");
+                                    if (!string.IsNullOrEmpty(isrc))
+                                    {
+                                        var nameStart = isrc.LastIndexOf('/');
+                                        var nameEnd = isrc.LastIndexOf('.');
+
+                                        if (nameStart != -1 && nameEnd != -1)
+                                        {
+                                            var name = isrc.Substring(nameStart + 1, nameEnd - nameStart - 1);
+                                            var x = char.ConvertFromUtf32(int.Parse(name, NumberStyles.HexNumber));
+                                            buildedMessage += x;
+                                        }
+                                        else
+                                        {
+                                            //full image ? 
+                                        }
+
+                                    }
+                                }
+                            }
+
+                            MessageTypes messageType = MessageTypes.Other;
+                            if (string.IsNullOrWhiteSpace(MessageUsername))
+                            {
+                                messageType = MessageTypes.None;
+                            }
+                            else if (string.Equals(MessageUsername, App.Username))
+                            {
+                                messageType = MessageTypes.Self;
+                            }
+                            listView.Items.Add(new ChatMessage()
+                            {
+                                Message = HtmlEntity.DeEntitize(buildedMessage),
+                                UserID = MessageUsername,
+                                DisplayName = MessageDisplayUsername,
+                                MessageType = messageType
+                            });
+                        }
+                        #endregion
+                        #region MessageImg
+
+                        if (messageNode.LastChild.Name == "div")
+                        {
+                            var userImages = messageNode.LastChild.SelectNodes("a");
+                            if (userImages != null && userImages.Count > 0)
+                            {
+
+                            }
+                            else
+                            {
+                                var otherImages = messageNode.LastChild.SelectNodes("img");
+                                if (otherImages != null)
+                                {
+                                    foreach (var otherImage in otherImages)
+                                    {
+                                        var imgSrc = otherImage.GetAttributeValue("src", "");
+                                        listView.Items.Add(new ChatMessage()
+                                        {
+                                            Message = HtmlEntity.DeEntitize(otherImage.GetAttributeValue("alt", "")),
+                                            MessageType = MessageTypes.Img,
+                                            UserID = MessageUsername,
+                                            DisplayName = MessageDisplayUsername,
+                                        });
+
+                                    }
+                                }
+                            }
+                        }
+                        #endregion
                     }
                 }
-                else if (!string.IsNullOrEmpty(node.InnerText))
+                else if (!string.IsNullOrEmpty(messagePackNode.InnerText))
                 {
-                    listView.Items.Add(new ChatMessage() { Message = HtmlEntity.DeEntitize(node.InnerText), UserID = MessageUsername, DisplayName = MessageDisplayUsername });
+                    listView.Items.Add(new ChatMessage() { Message = HtmlEntity.DeEntitize(messagePackNode.InnerText), MessageType = MessageTypes.None });
                 }
                 //   Names.Add(new ChatHeader() { Name = NameNode.InnerText, Href = NameNode.GetAttributeValue("href", "") });
             }
@@ -112,7 +226,7 @@ namespace App3
 
         private async void Send_Click(object sender, RoutedEventArgs e)
         {
-            listView.Items.Add(new ChatMessage() { Message = NewMessageBox.Text, UserID = "sidou", DisplayName = "sidou" });
+            listView.Items.Add(new ChatMessage() { Message = NewMessageBox.Text, UserID = App.Username, DisplayName = App.Username });
 
 
             Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
@@ -122,22 +236,27 @@ namespace App3
             HttpRequestMessage HttpRequestMessage =
                 new HttpRequestMessage(HttpMethod.Post,
                     new Uri(App.requestUri, _action));
-            HttpRequestMessage.Content=new HttpStringContent(_formEncoded += "&body="+ Uri.EscapeDataString(NewMessageBox.Text),UnicodeEncoding.Utf8,"application/x-www-form-urlencoded");
+            HttpRequestMessage.Content = new HttpStringContent(_formEncoded += "&body=" + Uri.EscapeDataString(NewMessageBox.Text), UnicodeEncoding.Utf8, "application/x-www-form-urlencoded");
             HttpRequestMessage.Headers.Add("User-Agent", App.CustomUserAgent);
+
+            NewMessageBox.Text = "";
+
             try
             {
                 //Send the GET request
                 httpResponse = await httpClient.SendRequestAsync(HttpRequestMessage);
                 httpResponse.EnsureSuccessStatusCode();
                 httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(httpResponseBody);
+                RefreshConversation(null, htmlDoc);
             }
             catch (Exception ex)
             {
                 httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
             }
-            //var htmlDoc = new HtmlDocument();
-            //htmlDoc.LoadHtml(httpResponseBody);
-          
+
+
 
 
 
@@ -148,7 +267,7 @@ namespace App3
 
         private void NewMessageBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Send.IsEnabled= !string.IsNullOrEmpty(NewMessageBox.Text);
+            Send.IsEnabled = !string.IsNullOrEmpty(NewMessageBox.Text);
         }
     }
 
